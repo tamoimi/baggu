0549accdf04a527b8574042c957eb3cbb80a9715
Tami <xorud7979@gmail.com>
2022-07-12 08:38:03 +0900

    .

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+/* scroll_menu */
+const header = document.querySelector('.scroll_header')
+
+window.addEventListener('scroll', ()=> {
+  if(window.scrollY>200){
+    gsap.to(header, 0.3, {
+      opacity: 1
+    })
+  }else {
+    gsap.to(header, 0.3, {
+      opacity: 0
+    })
+  }
+})
+
+
+
 /* btn */
 const topBtn = document.querySelector('.toTop')
 
